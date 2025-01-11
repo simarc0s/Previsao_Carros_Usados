acf44811525e8813edd61cab34353fa939fbf0d2
Simão Marcos <149081935+simarc0s@users.noreply.github.com>
2025-01-11 19:15:30 +0000

    modifiquei o notebook

--- scripts/preprocessing.py
+++ scripts/preprocessing.py
@@ -1,5 +1,4 @@
 import numpy as np
-import pandas as pd
 from sklearn.compose import ColumnTransformer
 from sklearn.feature_selection import mutual_info_regression
 from sklearn.model_selection import train_test_split
